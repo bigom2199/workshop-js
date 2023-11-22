2fd709d60506ff502f204cb755c059ef6f17635f
Bigom <bigom2199@gmail.com>
2023-11-22 14:39:04 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,49 @@
+// 
+
+console.log("hej");
+
+
+const date = new Date();
+let day = date.getDate();
+
+console.log(date);
+let month = date.getMonth()
+let year = date.getFullYear();
+let fullDate = `${day} -${month} -${year}`;
+
+
+console.log(fullDate);
+
+
+
+const todayDate = date.toJSON();
+console.log(todayDate.slice(0,12));
+
+
+
+function sundayOnJanuaryFirstday(){
+    for (let year = 2014; year<= 2050;year++);
+    const date = new date();
+    if (date.getDate=== 0){
+        console.log(`sunday is a januaryFirstDay and year`);
+    }
+}
+        
+function calculate(num1,num2, operation){
+    if(operation=== multiply)
+
+    
+
+function multiply(x, y){
+    return 12*10; // 120
+
+}
+function divide(x , y){
+    if (y === 0){
+        throw new Error("can not devide be zero");
+
+
+    }
+    return x / y ; // 12 / 10 
+}
+}
